9a8edd455f74b9d896115aca0c0d4cc5e739837a
David Carranza <david.carranza.ca@gmail.com>
2018-02-22 11:42:17 -0800

    adding more AngularJS examples

---
+++ AngularJS/leads.controller.js
@@ -0,0 +1,27 @@
+// CODE OMITTED FOR SECURITY
+
+function ProviderServiceDropdownCtrl(providerServiceService, roles, $stateParams) {
+    let $ctrl = this
+    $ctrl.roles = roles
+    $ctrl.providerServiceService = providerServiceService
+    $ctrl.id = null
+    $ctrl.currentServiceId = null
+
+    // if the variable $ctrl.providerId does exits when it comes into this component
+    // run the service call with the optional query
+    // Success and Error handling 
+    $ctrl.$onChanges = function() {
+        if($ctrl.providerId) {
+            providerServiceService.readAll(`?providerId=${$ctrl.providerId}`)
+            .then(onSuccess)
+            .catch(onError) 
+        }
+    }
+    function onSuccess(data) {
+        $ctrl.providerServices = data.items
+    }
+
+    function onError() {
+        console.log('error')
+    }
+}
