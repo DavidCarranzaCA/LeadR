efeb1906b716c2f45dbd85936845b56054ecba1e
David Carranza <david.carranza.ca@gmail.com>
2018-02-26 14:36:26 -0800

    Pushing latest changes

--- NodeJS/leadr.js
+++ NodeJS/leadr.js
@@ -1,42 +1,14 @@
-////
-function disburse(req, res) {
-    let batchSelectedLeads = {};
-    batchSelectedLeads.selectedLeadsArray = req.model;
+const ErrorResponse = require('../models/responses/error.response')
+const userService = require('../services/user.service')
+const userController = require('../controllers/users.controller')
 
-    leadsService.paypalBatch(batchSelectedLeads)
-        .then(id => {
-            return paypalService.disbursePayment(batchSelectedLeads.selectedLeadsArray, id)
-                .then(payout => {
-                    return leadsService.updateBatchPayout(id, payout.batch_header.payout_batch_id)
-                        .then(result => {
-                            return leadsService.updatePaidLeadsStatus(batchSelectedLeads.selectedLeadsArray, payout.batch_header.payout_batch_id)
-                                .then(result => {
-                                    console.log("Disburse Payments Success")
-                                    res.json(new responses.SuccessResponse())
-                                })
-                                .catch(err => res.send(new responses.ErrorResponse(err)));
-                        })
-                        .catch(err => console.log(err));
-                })
-                .catch(err => console.log(err));
-        })
-        .catch(err => console.log(err));
+module.exports = {
+    authenticate: authenticate,
+    writeUserId: writeUserId,
+    validateRole: validateRole,
+    authorizeRoleUpdate: authorizeRoleUpdate
 }
 
-function _login(req, res) {
-    return userService.login(req.model)
-        .then(user => {
-            if (_isValidUser(req.model.password, user)) {
-                helpers.sendAuthCookie.bind(null, req, res, user)()
-            } else {
-                res.status(401).send(new responses.ErrorResponse('Invalid Account Information'))
-            }
-        })
-        .catch(err => {
-            console.log(err)
-            res.status(500).send(new responses.ErrorResponse(err))
-        })
-}
 
 function authenticate(req, res, next) {
 
@@ -69,14 +41,17 @@ function authenticate(req, res, next) {
                 return false
             }
         }
+    } 
+    else if (req.cookies.auth && !req.cookies.auth_hash) {
+
+        res.clearCookie('auth_hash')
+        res.clearCookie('auth')
+        res.sendStatus(401)
+
+    } 
+    else {
+        return next()
     }
-    ///////// Omitted for security 
-    ///////// Omitted for security
-    ///////// Omitted for security
-    ///////// Omitted for security
-    ///////// Omitted for security
-    ///////// Omitted for security
-    ///////// Omitted for security
-    ///////// Omitted for security
-    ///////// Omitted for security
+    
 }
+
